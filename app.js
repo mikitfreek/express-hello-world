009a378eae8586ffb212089f7bb2e87a8fcc9b5d
mikitfreek <65701109+mikitfreek@users.noreply.github.com>
2023-12-09 04:59:33 +0100

    init

--- app.js
+++ app.js
@@ -12,32 +12,53 @@ app.use(function (req, res, next) {
 });
 
 // #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
+// Get url param, and send response
+app.get('/:param', function (req, res) {
+  
+  // get server data based on :param variable
+
+  const name = req.params.param;
+  const person = req.params.param;
+
+  const body = `
+<div style="text-align: center; height: 100%; color: #fbfbfb; font-family: 'Verdana'; background: rgb(131,58,180); background: linear-gradient(36deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);">
+  <div style="padding-top: 9%; font-size: 4.3em;">Heja ${name}!</div>
+  <div style="padding-top: 1.7em; font-size: 2.3em;">Twoim świątecznym losem jest:</div>
+  <div style="padding-top: .7em; font-size: 9em;">${person}</div>
+</div>`
+
+  // Prints Output on the browser in response 
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); 
+  res.write(body, 'utf8');
+  res.end(); 
+});
 
 // #############################################################################
 // Catch all handler for all other request.
 app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
+  // res.json({
+  //     at: new Date().toISOString(),
+  //     method: req.method,
+  //     hostname: req.hostname,
+  //     ip: req.ip,
+  //     query: req.query,
+  //     headers: req.headers,
+  //     cookies: req.cookies,
+  //     params: req.params
+  //   })
+  //   .end()
+
+  const body = `
+  <div style="text-align: center; height: 100%; color: #fbfbfb; font-family: 'Verdana'; background: rgb(131,58,180); background: linear-gradient(36deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);">
+    <div style="padding-top: 9%; font-size: 4.3em;">mordo,</div>
+    <div style="padding-top: 1.7em; font-size: 2.3em;">coś źle link spasował</div>
+    <div style="padding-top: .7em; font-size: 9em;">BO NIE DZIAŁA</div>
+  </div>`
+  
+    // Prints Output on the browser in response 
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); 
+    res.write(body, 'utf8');
+    res.end(); 
 })
 
 module.exports = app
